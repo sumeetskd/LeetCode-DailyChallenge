0b76cdab85eaf8612dc763b08b11c9b560a5b1bb
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-06-02 03:39:16 +0530

    Create 1091_Shortest_Path_in_Binary_Matrix.java

---
+++ June/Day 1/1091_Shortest_Path_in_Binary_Matrix.java
@@ -0,0 +1,39 @@
+class Solution {
+    int ROW, COL;
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        ROW = grid.length;
+        COL = grid[0].length;
+        
+        if (grid[0][0] != 0 || grid[ROW - 1][COL - 1] != 0) {
+            return -1;
+        }
+
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] dirs = new int[][]{{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1}};
+        q.offer(new int[]{0, 0});
+        grid[0][0] = 1;
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int row = cur[0];
+            int col = cur[1];
+            int distance = grid[row][col];
+            if(row == ROW-1 && col == COL-1) {
+                return distance;
+            }
+            for(int[] dir : dirs) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                if(isBound(newRow, newCol) && grid[newRow][newCol] == 0) {
+                    q.offer(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = distance + 1;
+                }
+            }
+        }
+        // destination unreachable
+        return -1;
+    }
+
+    boolean isBound(int row, int col) {
+        return row>=0 && row<ROW && col>=0 && col<COL;
+    }
+}
